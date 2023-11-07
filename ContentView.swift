1ca56abdd908194950ee0557df122b65926a4760
Daniel Seither <d@fdseither.de>
2023-11-07 11:26:47 +0100

    Add workspace, project, package, and dependabot config

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DependabotDemo
+//
+//  Created by Daniel Seither on 07.11.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DependabotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependabotDemoApp.swift
+//  DependabotDemo
+//
+//  Created by Daniel Seither on 07.11.23.
+//
+
+import SwiftUI
+
+@main
+struct DependabotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
